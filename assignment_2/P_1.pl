d3c50f12e528c6cc8573827004d0fba5834d0e1b
Sambhav Kothari <sambhavs.email@gmail.com>
2017-10-20 03:11:17 +0530

    Add code for problem 1

---
+++ assignment_2/P_1.pl
@@ -0,0 +1,23 @@
+parent(jatin,avantika).
+parent(jolly,jatin).
+parent(jolly,kattappa).
+parent(manisha,avantika).
+parent(manisha,shivkami).
+parent(bahubali,shivkami).
+male(kattappa).
+male(jolly).
+male(bahubali).
+female(shivkami).
+female(avantika).
+grandparent(X, Y) :-
+    parent(X, Z), parent(Z, Y).
+uncle(X, Y) :-
+    male(X), grandparent(Z, Y), parent(Z, X).
+halfsister(X, Y) :-
+    X \== Y,
+    female(X),
+    parent(A, X), parent(A, Y),     
+    parent(C, X), parent(D, Y),
+    A \== C,
+    A \== D,
+    C \== D. 
