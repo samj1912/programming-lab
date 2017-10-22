84998c0ed5cc7afd2628a3b55710e4e2b702da20
Sambhav Kothari <sambhavs.email@gmail.com>
2017-10-22 19:25:34 +0530

    Fix formatting and add comments

--- assignment_2/P_1.pl
+++ assignment_2/P_1.pl
@@ -1,23 +1,43 @@
+%% -----------------
+%% Relationship Data
+%% -----------------
 parent(jatin,avantika).
 parent(jolly,jatin).
 parent(jolly,kattappa).
 parent(manisha,avantika).
 parent(manisha,shivkami).
 parent(bahubali,shivkami).
+
+%% -----------
+%% Gender data
+%% -----------
+male(jatin).
 male(kattappa).
 male(jolly).
 male(bahubali).
+female(manisha).
 female(shivkami).
 female(avantika).
-grandparent(X, Y) :-
+
+%% ----------------------------------------
+%% Rules for determining Uncle Relationship
+%% ----------------------------------------
+grand_parent(X, Y) :-
     parent(X, Z), parent(Z, Y).
+
 uncle(X, Y) :-
-    male(X), grandparent(Z, Y), parent(Z, X), \+parent(X,Y).
-halfsister(X, Y) :-
+    male(X),
+    grand_parent(Z, Y), parent(Z, X),
+    \+ parent(X, Y).
+
+%% ----------------------------------------------
+%% Rules for determining Half-Sister Relationship
+%% ----------------------------------------------
+half_sister(X, Y) :-
     X \== Y,
     female(X),
     parent(A, X), parent(A, Y),     
     parent(C, X), parent(D, Y),
     A \== C,
     A \== D,
-    C \== D. 
+    C \== D.
